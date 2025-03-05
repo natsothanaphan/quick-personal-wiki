61992194079ee017fd2802bbc5d0ac370161613e
Nat Sothanaphan <natsothanaphan@gmail.com>
2025-03-05 15:55:53 +0700

    skeleton

---
+++ src/components/MainPage.jsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import './MainPage.css';
+import api from '../api.js';
+import { alertAndLogErr } from '../utils.js';
+
+const MainPage = ({ user }) => {
+  return <div>MainPage</div>;
+};
+
+export default MainPage;
